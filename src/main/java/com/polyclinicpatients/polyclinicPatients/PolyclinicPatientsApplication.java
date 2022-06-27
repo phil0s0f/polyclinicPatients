18f6a096a0bfae6a4b35c837edad692b578c900b
Дугаров Вячеслав <sdugharow@yandex.ru>
2022-06-27 22:46:19 +0800

    добавлены задачи

--- src/main/java/com/polyclinicpatients/polyclinicPatients/PolyclinicPatientsApplication.java
+++ src/main/java/com/polyclinicpatients/polyclinicPatients/PolyclinicPatientsApplication.java
@@ -8,6 +8,8 @@ public class PolyclinicPatientsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PolyclinicPatientsApplication.class, args);
+		//TODO: изменить doctor-details,patient-details, скорее всего это должно быть в контроллере Visit
+		//TODO: подумать над ссылками в doctor-main, patient-main, они должны перекидывать на Visit
 	}
 
 }
