da73c3bbba3e297dfb6630c65ccb46c9c0953d31
Дугаров Вячеслав <sdugharow@yandex.ru>
2022-06-29 12:40:01 +0800

    доработан VisitRepository

--- src/main/java/com/polyclinicpatients/polyclinicPatients/rep/VisitRepository.java
+++ src/main/java/com/polyclinicpatients/polyclinicPatients/rep/VisitRepository.java
@@ -3,5 +3,9 @@ package com.polyclinicpatients.polyclinicPatients.rep;
 import com.polyclinicpatients.polyclinicPatients.models.Visit;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface VisitRepository extends CrudRepository<Visit, Long> {
+    //TODO: придется переделать модели Doctor, Patient
+    //List<Visit> findByPatient_Id_patient(Long id);
 }
